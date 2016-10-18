1fc6ce3b76e0d2e77bab776a378fcc385a3febea
Jasdero <houssemaine.j@gmail.com>
2016-10-18 15:20:17 +0200

    creation base formulaire

---
+++ src/FrontBundle/Controller/BaseCommuneController.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace FrontBundle\Controller;
+
+use FrontBundle\Entity\BaseCommune;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+
+class BaseCommuneController extends Controller
+{
+    public function newForm (Request $request)
+    {
+
+    }
+
+}
