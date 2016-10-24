9e2708fae1fe2ab4c67e2063f3440741cd868c6d
Jasdero <houssemaine.j@gmail.com>
2016-10-24 13:23:25 +0200

    step course pedestre fait

--- src/FrontBundle/Form/CreatePosteSecoursStep5Form.php
+++ src/FrontBundle/Form/CreatePosteSecoursStep5Form.php
@@ -8,6 +8,8 @@
 
 namespace FrontBundle\Form;
 
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -16,15 +18,54 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class CreatePosteSecoursStep5Form extends AbstractType
 {
+
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+
+    //Formulaire courses pédestres
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('tailleStade');
+            ->add('typeCourse', ChoiceType::class, array('label'=>'Type de course pédestre',
+                'choices'=> array(
+                    'Trail'=>'trail',
+                    'Run and Bike'=>'run and bike',
+                    'Courses'=>'courses',
+                    'Autre'=>'autre'),
+                'choices_as_values' => true,
+            ))
+            ->add('distParcours1', NumberType::class, array('label'=>'Distance du parcours 1 en km'))
+            ->add('distParcours2', NumberType::class, array('label'=>'Distance du parcours 2 en km'))
+            ->add('obstacles', ChoiceType::class, array('label'=>'Présence d\'obstacles',
+                'choices'=> array(
+                    'Oui'=>'oui',
+                    'Non'=>'non'),
+                'choices_as_values' => true,
+            ))
+            ->add('signaleur', ChoiceType::class, array('label'=>'Présence de signaleur',
+                'choices'=> array(
+                    'Oui'=>'oui',
+                    'Non'=>'non'),
+                'choices_as_values' => true,
+            ))
+            ->add('typePiste', ChoiceType::class, array('label'=>'Type de voie',
+                'choices'=> array(
+                    'Piste'=>'piste',
+                    'Chemin privé'=>'chemin privé',
+                    'Voie publique'=>'voie publique'),
+                'choices_as_values' => true,
+            ))
+            ->add('commentaire', TextareaType::class, array('label'=>'Commentaire'))
+            ->add('save', SubmitType::class, array('label' => 'Envoyer'))
+        ;
     }
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
-            'data_class' => 'FrontBundle\Entity\PosteSecours'
+            'data_class' => 'FrontBundle\Entity\FormulaireSecours'
         ));
     }
     public function getBlockPrefix()
