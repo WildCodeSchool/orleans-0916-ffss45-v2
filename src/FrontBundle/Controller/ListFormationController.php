7f84246f2649a14637df983595a6819a0ce9bf17
Guillaume4545 <guillaumecuriel@gmail.com>
2016-10-27 16:37:51 +0200

    Merge branch 'master' of https://github.com/WildCodeSchool/orleans-0916-ffss45
    # Conflicts:
    #	src/FrontBundle/Controller/DefaultController.php

--- src/FrontBundle/Controller/ListFormationController.php
+++ src/FrontBundle/Controller/ListFormationController.php
@@ -12,17 +12,23 @@ use AdminBundle\Entity\FormationPublic;
 class ListFormationController extends Controller
 {
     /**
-     * @Route("/formations/{typeformation}", name="acceuil")
+     * @Route("/formations/{typeFormation}", name="acceuil")
      */
     public function indexAction($typeFormation)
     {
-
+        $formations='';
+       // $typeFormation='';
         $em = $this->getDoctrine()->getManager();
-        $categories = $em->getRepository('AdminBundle:FormationType')->findByType($typeFormation);
-        //$categories = $em->getRepository('AdminBundle:Categorie')->findAll();
-        foreach ($categories as $categorie) {
-            $nomCategorie = $categorie->getNomCategorie();
-            $formations[$nomCategorie] = $em->getRepository('AdminBundle:Formation')->findByCategorie(array($categorie));
+        $typeFormations = $em->getRepository('AdminBundle:FormationPublic')->findByType($typeFormation);
+      //  var_dump($typeFormations);
+        foreach($typeFormations as $typeFormation) {
+            $categorie = $typeFormation->getCategorie();
+            //$typeFormation and $categorie-> findAll($formations);
+            // $categories = $em->getRepository('AdminBundle:Categorie')->findAll();
+            //foreach ($categories as $categorie) {
+                $nomCategorie = $categorie->getNomCategorie();
+                $formations[$nomCategorie] = $em->getRepository('AdminBundle:Formation')->findByCategorie(array($categorie));
+            //}
         }
         return $this->render('FrontBundle:ListFormation:index.html.twig', array('formations'=>$formations));
 
