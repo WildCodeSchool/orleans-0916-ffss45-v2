3380e4bb1df3254ed4425160a01b46a76f63a5b4
PatrickS45 <tuko45@hotmail.fr>
2016-12-09 16:42:26 +0100

    Ajout suppression + Quantité ok

--- src/CommerceBundle/Controller/PanierController.php
+++ src/CommerceBundle/Controller/PanierController.php
@@ -37,7 +37,6 @@ class PanierController extends Controller
      * @Route("/panier", name="panier")
      */
     public function panierAction(Request $request)
-
     {
 
         $em = $this->getDoctrine()->getManager();
@@ -49,27 +48,27 @@ class PanierController extends Controller
         }
 
         $panier = "";
-        $total = 0;
+        $totalfinal = 0;
         foreach ($session->get('panier') as $id => $qte) {
 
             $agenda = $em->getRepository('AdminBundle:Agenda')->find($id);
-            $panier[] = array('agenda' => $agenda, 'quantite' => $qte);
-            $total+=$agenda->getFormation()->getPrix()*$qte;
+            $panier[] = array('agenda' => $agenda, 'quantite' => $qte, 'totalitem' => $agenda->getFormation()->getPrix()*$qte);
+            $totalfinal+=$agenda->getFormation()->getPrix()*$qte;
 
         }
         //var_dump($panier);
         return $this->render('@Commerce/Default/panier.html.twig', array(
-            'panier' => $panier,  'total' => $total
+            'panier' => $panier,  'totalfinal' => $totalfinal
 
         ));
     }
 
     /**
-     * @Route("/quantityForm", name="qtform")
+     * @Route("/quantityForm/{id}", name="qtform")
      */
     public function quantityFormAction($id, Request $request)
     {
-        for ($i = 1; $i<=10; $i++){
+        for ($i = 1; $i<=20; $i++){
             $choices[$i]=$i;
         }
 
@@ -82,16 +81,28 @@ class PanierController extends Controller
             //->add('id', HiddenType::class, array('data' => $id))
             ->getForm();
         ;
-
         $form->handleRequest($request);
-
         if ($form->isSubmitted() && $form->isValid()) {
             $data = $form->getData();
-            $panier[$id]=$data->getQte();
+            $panier[$id]=$data->getQuantity();
+            $session->set('panier',$panier);
+            return $this->redirect($this->generateUrl('panier'));
         }
-        return $this->render('@Commerce/Default/quantityForm.html.twig', array(
-            'form' => $form->createView(),
-
+         return $this->render('@Commerce/Default/quantityForm.html.twig', array(
+        'form' => $form->createView(),
+        'id'=>$id
         ));
+    }
+
+    /**
+     * @Route("/remove/{id}", name="remove")
+     */
+    public function deleteFunction($id, Request $request)
+    {
+        $session = $request->getSession();
+        $panier = $session->get('panier');
+        unset($panier[$id]);
+        $session->set('panier',$panier);
+        return $this->redirect($this->generateUrl('panier'));
     }
 }
