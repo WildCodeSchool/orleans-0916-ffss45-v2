9931123e50702f1ecfa61edd04c9f0dc3fae8348
Gy0m <landreaug@gmail.com>
2017-01-05 17:58:28 +0100

    profile edit surcharge fosuser

--- src/AdminBundle/Entity/User.php
+++ src/AdminBundle/Entity/User.php
@@ -7,6 +7,7 @@ use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
+
 /**
  * @ORM\Entity
  * @ORM\Table(name="fos_user")
