395cc0c1dfb3c3012207d907877854b766a49bb6
Jpilosel <formation@pilosel.fr>
2017-01-24 14:59:49 +0100

    ajout Captchat sur le formulaire de l'acceuil

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -27,6 +27,7 @@ class AppKernel extends Kernel
             new CommerceBundle\CommerceBundle(),
             new Tlconseil\SystempayBundle\TlconseilSystempayBundle(),
             new Vich\UploaderBundle\VichUploaderBundle(),
+            new Gregwar\CaptchaBundle\GregwarCaptchaBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
