b1f00e6d23b5a0550e1cc0eda133977fcdf63f2a
Jpilosel <formation@pilosel.fr>
2017-01-31 14:37:40 +0100

    $orderId

--- src/CommerceBundle/Controller/PanierController.php
+++ src/CommerceBundle/Controller/PanierController.php
@@ -483,12 +483,12 @@ class PanierController extends Controller
             $totalfinal += $panier[$agenda->getId()]['totalitem'] + $totalLivraison;
 
         }
-
+        $orderId = uniqid(1, false);
         $systempay = $this->get('tlconseil.systempay')
             ->init($currency = 978, $amount = ($totalfinal*100))
             ->setOptionnalFields(array(
                 'shop_url' => 'http://193.70.38.206/ffss45/app.php',
-                'order_id' => '12345'
+                'order_id' => $orderId
             ));
 
         return array(
