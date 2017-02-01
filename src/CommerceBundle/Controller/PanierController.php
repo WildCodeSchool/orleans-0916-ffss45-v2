de8a20d50ba9f290c5ea9833ee5b15d80abf5b9e
PatrickS45 <tuko45@hotmail.fr>
2017-02-01 14:55:57 +0100

    avant pull

--- src/CommerceBundle/Controller/PanierController.php
+++ src/CommerceBundle/Controller/PanierController.php
@@ -293,7 +293,7 @@ class PanierController extends Controller
 
 
         }
-        $em->flush();die;
+        $em->flush();
         return $this->redirect($this->generateUrl('page_accueil_principale'));
         // }
         // return $this->render('@Front/Default/acceuil.html.twig', array(
